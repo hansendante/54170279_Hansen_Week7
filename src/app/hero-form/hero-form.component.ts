3ef9f7bd6dcb0d84e0fb41b2347a6376c523ecb5
hansendante <hansenreizo@gmail.com>
2020-03-25 21:24:59 +0700

    ''

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { DataService } from '../data.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.scss']
+})
+export class HeroFormComponent implements OnInit {
+  hero: Hero = {
+    _id: '',
+    heroName: '',
+    realName: '',
+    universe: 'MCU',
+    skill: ''
+  };
+  id = null;
+  error = false;
+  update = true;
+
+  constructor(
+    private ds: DataService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      // jika ada parameter id di URL
+      if (params.get('id')) {
+        this.id = params.get('id');
+
+        this.ds.getHero(this.id).subscribe(
+          response => {
+            this.hero = response as Hero;
+            
+          },
+          err => {
+            console.log(err);
+            this.error = true;
+          }
+        );
+      } else {
+        this.update = false;
+      }
+    });
+  }
+
+  postHero() {
+    this.ds.postHero(this.hero).subscribe(response => {
+      // tampilkan notifikasi
+      alert("Hero Added");
+      this.router.navigate(['/main']);
+    });
+    }
+
+  deleteHero() {
+    this.ds.deleteHero(this.hero).subscribe(
+      response => {
+        // tampilkan notifikasi
+        alert("Hero Deleted");
+        this.router.navigate(['/main']);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  updateHero() {
+    this.ds.updateHero(this.hero).subscribe(
+      response => {
+        // tampilkan notifikasi
+        alert("Hero Updated");
+        this.router.navigate(['/main']);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+}
